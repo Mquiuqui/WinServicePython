e2052c75a9902487a42cb54768d888f185f88384
Mquiuqui <Marcosaquiuqui@gmail.com>
2021-07-22 20:52:49 -0300

    layout arrumado

--- main.py
+++ main.py
@@ -6,27 +6,55 @@ from win32printing import Printer
 
 def writeToFile():
 
+
     font = {
-        "height": 11,
+        "height": 12,
+        "orientation": 'left',
 
     }
-    with Printer() as printer:
+    font2 = {
+        "height": 13,
+        "orientation": 2,
 
-        printer.text("   Transportadora\n"+"   rodrigo", font_config=font)
-        printer.text(" ", font_config=font)
-        printer.text("   Data\n"+"   R 00/00/2021 21:21:08", font_config=font)
-        printer.text(" ", font_config=font)
-        printer.text("   Nome motorista e visto.", font_config=font)
-        printer.text("_____________________________________________________________", font_config=font)
-        printer.text("   Diluição", font_config=font)
-        printer.end_page
+    }
+
+
+    T = "Tansportadora".ljust(30)
+    D = "Data".ljust(34)
+    N = "Nome motorista e visto".ljust(5)
+    Di = "Diluição:"
+    O = "080_FOPE-REV00"
+
+
+    t = T.center(36,' ')
+    d = D.center(41,' ')
+    n = N.center(30,' ')
+    di = Di.center(10,' ')
+
+    nome = "Transportadora Quiuqui".rjust(3)
+    data = "20/02/2021".center(17,' ')
+    placa = "CUD-0056".center(0,' ') 
+
+
+
+
+        
+    with Printer(printer_name="HP Photosmart C4700 series") as printer:
+            printer.text(t+"Remessa:%s"%("20000/21"), font_config=font)
+            printer.text("    "+nome, font_config=font)
+            printer.text(d+" Efluente:%s"%("Fossa Séptica\n")+data, font_config=font)
+            printer.text(n+" Placa:"+placa+"\n"+"___________________________________________________", font_config=font)
+            printer.text("    "+di+"40     "+O,)
+
+
+            printer.end_page
 
 
 
 
 class PythonCornerExample(SMWinservice):
-    _svc_name_ = "teste_quiuqui"
-    _svc_display_name_ = "teste6"
+    _svc_name_ = "teste_quiuqui7"
+    _svc_display_name_ = "teste7"
     _svc_description_ = "descrição qualquer sobre impressão"
 
     def start(self):
--- testeimpre.py
+++ testeimpre.py
@@ -1,8 +1,4 @@
 from win32printing import Printer, PrinterFontContext
-a = "testeRemessa"
-b = "testeEfluente"
-c = "testePlaca"
-d = ""
 
 font = {
     "height": 12,
@@ -16,19 +12,19 @@ font2 = {
 }
 
 
-T = " Tansportadora".ljust(30)
+T = "Tansportadora".ljust(30)
 D = "Data".ljust(34)
 N = "Nome motorista e visto".ljust(5)
 Di = "Diluição:"
 O = "080_FOPE-REV00"
 
 
-t = T.center(35,' ')
+t = T.center(36,' ')
 d = D.center(41,' ')
 n = N.center(30,' ')
 di = Di.center(10,' ')
 
-nome = "Rodrigo".center(16, ' ')
+nome = "Transportadora Quiuqui".rjust(3)
 data = "20/02/2021".center(17,' ')
 placa = "CUD-0056".center(0,' ') 
 
@@ -36,10 +32,11 @@ placa = "CUD-0056".center(0,' ')
 
 
     
-with Printer() as printer:
-        printer.text(t+"Remessa:%s"%("20000/21\n"+nome), font_config=font)
-        printer.text(d+"Efluente:%s"%("Fossa Séptica\n")+data, font_config=font)
-        printer.text(n+"Placa:"+placa+"\n"+"___________________________________________________", font_config=font)
+with Printer(printer_name="HP Photosmart C4700 series") as printer:
+        printer.text(t+"Remessa:%s"%("20000/21"), font_config=font)
+        printer.text("    "+nome, font_config=font)
+        printer.text(d+" Efluente:%s"%("Fossa Séptica\n")+data, font_config=font)
+        printer.text(n+" Placa:"+placa+"\n"+"___________________________________________________", font_config=font)
         printer.text("    "+di+"40     "+O,)
 
 
